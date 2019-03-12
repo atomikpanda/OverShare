d67949ee2bdfa703a29d43fd05198f82548433f5
Bailey Seymour (atomikpanda) <b.seymour@me.com>
2019-03-12 12:45:22 -0400

    create stub activity for receiving

--- OverShare/app/src/main/java/com/baileyseymour/overshare/activities/ReceiveActivity.java
+++ OverShare/app/src/main/java/com/baileyseymour/overshare/activities/ReceiveActivity.java
@@ -1,4 +1,29 @@
+// Bailey Seymour
+// DVP6 - 1903
+// ReceiveActivity.java
+
 package com.baileyseymour.overshare.activities;
 
-public class ReceiveActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baileyseymour.overshare.R;
+import com.baileyseymour.overshare.fragments.ReceiveFragment;
+
+import butterknife.ButterKnife;
+
+public class ReceiveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_container_layout);
+        ButterKnife.bind(this);
+
+        // Load the fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer,
+                        ReceiveFragment.newInstance())
+                .commit();
+    }
 }
